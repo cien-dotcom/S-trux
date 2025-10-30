78023278ae830552b293f75ba3e6687c3ac67b59
Cindra Bunga <cindrabp@gmail.com>
2025-10-31 06:27:12 +0700

    tes

--- components/hello-wave.tsx
+++ components/hello-wave.tsx
@@ -1,18 +1,37 @@
-import Animated from 'react-native-reanimated';
+import React, { useEffect } from 'react';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withRepeat,
+} from 'react-native-reanimated';
 
 export function HelloWave() {
+  const rotation = useSharedValue(0);
+
+  useEffect(() => {
+    rotation.value = withRepeat(
+      withTiming(25, { duration: 300 }), // rotasi 25 derajat
+      4, // ulang 4 kali
+      true // bolak-balik (yoyo)
+    );
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${rotation.value}deg` }],
+  }));
+
   return (
     <Animated.Text
-      style={{
-        fontSize: 28,
-        lineHeight: 32,
-        marginTop: -6,
-        animationName: {
-          '50%': { transform: [{ rotate: '25deg' }] },
+      style={[
+        {
+          fontSize: 28,
+          lineHeight: 32,
+          marginTop: -6,
         },
-        animationIterationCount: 4,
-        animationDuration: '300ms',
-      }}>
+        animatedStyle,
+      ]}
+    >
       👋
     </Animated.Text>
   );
